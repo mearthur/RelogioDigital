51e8b5dd3219ee3d46e9ebc3702dcdf4b0f34728
ATR {ARTHUR ARAUJO} <104591770+mearthur@users.noreply.github.com>
2023-03-15 16:21:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ const relogio = setInterval(function time() {
   
   if (6 <= hr && hr < 12) {
     document.body.style.background =
-      "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+      "linear-gradient(34deg, rgba(255,0,0,1) 5%, rgba(255,85,0,1) 32%, rgba(255,226,0,1) 91%)";
   } else if (12 <= hr && hr < 18) {
     document.body.style.background =
-    "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+      "linear-gradient(34deg, rgba(0,6,122,1) 11%, rgba(49,52,201,1) 51%, rgba(160,255,227,1) 90%)";
   } else {
     document.body.style.background =
-    "linear-gradient(227deg, rgba(0,255,162,1) 26%, rgba(0,255,64,1) 90%)";
+      "linear-gradient(34deg, rgba(0,3,50,1) 52%, rgba(207,23,23,1) 85%, rgba(255,154,0,1) 100%)";
   }
 });
 
